3569e62b1f1ce099da5df17e5eee6a8b0378b119
binodsingh1982 <31635895+binodsingh1982@users.noreply.github.com>
2019-03-16 09:16:24 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,55 +1,50 @@
 package com.appian;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
 		Deck deck = new Deck();
 		Card[] cards = new Card[52];
 		for (int i = 0; i < 52; i++) {
 			cards[i] = deck.dealOneCard();
-			
+
 			System.out.printf("original %s of %s\n", cards[i].getRank(), cards[i].getSuit());
 		}
-	deck.reset();	
-deck.shuffle();
+		deck.reset();
+		deck.shuffle();
 		for (int i = 0; i < 52; i++) {
 			cards[i] = deck.dealOneCard();
-			
+
 			System.out.printf("After first shuffle %s of %s\n", cards[i].getRank(), cards[i].getSuit());
 		}
-		
+
 		deck.reset();
-		
-		
 
 		for (int i = 0; i < 52; i++) {
 			cards[i] = deck.dealOneCard();
-			
+
 			System.out.printf("After reset %s of %s\n", cards[i].getRank(), cards[i].getSuit());
 		}
-		
+
 		deck.reset();
 		deck.shuffle();
-		
-		
-		
-	Card card=	deck.dealOneCard();
-		
+
+		Card card = deck.dealOneCard();
+
 		System.out.printf("After shuffle one card %s of %s\n", card.getRank(), card.getSuit());
-		
+
 		deck.shuffle();
-		
-		card=	deck.dealOneCard();
-		
+
+		card = deck.dealOneCard();
+
 		System.out.printf("After re-shuffle one card %s of %s\n", card.getRank(), card.getSuit());
-		
+
 		for (int i = 0; i < 52; i++) {
 			cards[i] = deck.dealOneCard();
-			
+
 			System.out.printf("After re-shuffle %s of %s\n", cards[i].getRank(), cards[i].getSuit());
 		}
-		
-		
+
 	}
 
 }
